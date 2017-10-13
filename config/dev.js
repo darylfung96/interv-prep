d0dd2363016b45165dce59cf43f5633d65dc029a
Daryl Fung <daryl_chief@hotmail.com>
2017-10-13 18:55:16 -0500

    initial commit

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	CLIENT_ID: '615008100681-c0lng825sbtl6hkh4bn8t4kigcmugu1e.apps.googleusercontent.com',
-	CLIENT_SECRET: 'P3foJSe6Vtfwc47_68aj-qvX',
-	MONGO_URI: 'mongodb://ideathon:ideathon@ds111895.mlab.com:11895/ideathon',
-	COOKIE_KEY: 'jfsadjfjTOWIEURJFSO_$(#$JDLF-JRALKFJLGJASLKJAWLKEJ1134FJALSKDFJ)',
-
-	FACEBOOK_CLIENT_ID: '356931374731399',
-	FACEBOOK_CLIENT_SECRET: '3af68c68c5a18af5b76350491204f047',
-}
