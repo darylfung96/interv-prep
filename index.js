c606cf94c1f62c06237dee14e95bf957cd0d24f7
Daryl Fung <daryl_chief@hotmail.com>
2017-10-14 15:15:43 -0500

    added path to client server

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.use(passport.session());
 
 require('./routes/authRoute')(app);
 
-// const path = require('path');
-//
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// 	});
-// }
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 
 app.listen(process.env.PORT || 5000);
