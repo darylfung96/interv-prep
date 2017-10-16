b88343362fa6d5df17360d0e80142d4af5f50a47
Daryl Fung <darylfung96@gmail.com>
2017-10-15 22:13:29 -0500

    added some content in dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { Header } from './common';
 import { fetchUser } from '../actions';
+import { fontFamilyStyle } from '../styles/fontStyle';
 
 class Dashboard extends Component {
 	render() {
@@ -15,7 +16,25 @@ class Dashboard extends Component {
 		return (
 			<div>
 			<Header />
-				<p>Logged In</p>
+			<br/><br/><br/>
+			<h4 style={{ marginLeft: 15 }}>Interviews</h4>
+
+			<div className="collection">
+			  <a href="#!" className="collection-item blue-text text-darken-1" style={{ fontSize: 18 }}><span class="badge">1</span>Wawanesa</a>
+			  <a href="#!" className="collection-item blue-text text-darken-1" style={{ fontSize: 18 }}><span class="new badge">4</span>Norima</a>
+			  <a href="#!" className="collection-item blue-text text-darken-1" style={{ fontSize: 18 }}>24-7 InTouch</a>
+			  <a href="#!" className="collection-item blue-text text-darken-1" style={{ fontSize: 18 }}><span class="badge">14</span>Amazon</a>
+			</div>
+
+			<div class="fixed-action-btn horizontal">
+    			<a class="btn-floating btn-large blue lighten-1">
+      				<i class="large material-icons">mode_edit</i>
+    			</a>
+    			<ul>
+      				<li><a class="btn-floating red"><i class="material-icons">add</i></a></li>
+    			</ul>
+  			</div>
+
 			</div>
 		);
 	}
